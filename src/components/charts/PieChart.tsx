fe6cb6e625c35c372b2f8e03b7807e67f0a4f0bb
Harmeetrai <harmeetrai18@gmail.com>
2023-03-02 13:55:33 -0800

    update charts

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box, Typography, Stack } from '@pankod/refine-mui'
+import { PieChartProps } from 'interfaces/home'
 
-const PieChart = () => {
+const PieChart = ({ title, value, series, colors}: PieChartProps) => {
   return (
-    <div>PieChart</div>
+    <Box>
+      <Stack direction="column">
+        <Typography fontSize={14} color="#808191">
+          {title}
+        </Typography>
+        <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
